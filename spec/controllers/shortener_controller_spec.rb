6cf919d4286ca248d9003d0ab44b081b347fbd34
Dmitry <dmitry@yulchik.com>
2018-05-15 20:51:48 +0200

    Added functional tests

---
+++ spec/controllers/shortener_controller_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+RSpec.describe ShortenerController do
+  describe "Generate short url" do
+    subject { post("/") && response }
+
+    its(:status) { is_expected.to eq 200 }
+  end
+
+  describe "Should redirect from short to full url" do
+    let(:url) { "http://teeststs.com" }
+    let(:id) { ShortenerService.create(url) }
+
+    subject { get("/#{id}") && response }
+
+    it "should redirect to original url" do
+      expect(subject).to redirect_to(url)
+    end
+  end
+end
