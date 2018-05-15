c266718fb3c00487809d1f256dc0d8e88ef60a3e
Dmitry <dmitry@yulchik.com>
2018-05-15 20:02:24 +0200

    Implement Service and tests

---
+++ app/services/shortener_service.rb
@@ -0,0 +1,17 @@
+class ShortenerService
+  SHORTENER_BASE = 36
+
+  def self.create(url)
+    pack(ShortedUrl.first_or_create(url: url))
+  end
+
+  def self.restore(id)
+    ShortedUrl.find(id).url
+  end
+
+  private
+
+  def self.pack(shorted_url)
+    shorted_url.id.to_s(SHORTENER_BASE)
+  end
+end
---
+++ spec/services/shortener_service_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+RSpec.describe ShortenerService do
+  let(:url) { "http://test.com" }
+  let(:service) { described_class }
+
+  describe "when create is called" do
+    subject { service.create(url) }
+
+    it "should generate short url id" do
+      is_expected.to be_a_kind_of(String)
+    end
+
+    it "should generate the same id on second request" do
+      is_expected.to eq(service.create(url))
+    end
+  end
+
+  describe "when url is shortened" do
+    let(:url_id) { service.create(url) }
+    subject { service.restore(url_id) }
+
+    it "should able to restore" do
+      is_expected.to eq(url)
+    end
+  end
+end
